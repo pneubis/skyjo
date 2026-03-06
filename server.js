bd4ee63a45306442749a9c2852e3eca06c7f9306
pneubis <54792493+pneubis@users.noreply.github.com>
2026-03-06 22:22:57 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const { ExpressPeerServer } = require('peer');
+
+const app = express();
+const server = require('http').createServer(app);
+
+app.get('/', (req, res) => res.send('Skyjo PeerJS Server OK'));
+
+const peerServer = ExpressPeerServer(server, {
+  path: '/',
+  allow_discovery: true
+});
+
+app.use('/peerjs', peerServer);
+
+const PORT = process.env.PORT || 9000;
+server.listen(PORT, () => console.log('PeerJS server running on port', PORT));
